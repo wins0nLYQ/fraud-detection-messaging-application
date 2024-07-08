092234e5989996f31f192f1cb6672675738b6d8a
Winson Low <Winson@Winsons-MacBook-Pro.local>
2024-07-08 22:02:30 +0800

    User register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,12 @@
+import axios from "axios";
+import Register from "./Register"
+
 function App() {
+  axios.defaults.baseURL = 'http://localhost:3000';
+  axios.defaults.withCredentials = true;
+
   return (
-    <div className="bg-red-500">Hello</div>
+    <Register />
   )
 }
 
